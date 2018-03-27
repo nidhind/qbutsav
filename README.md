e6c92127ca6292783fcbadf404eab643441bfc73
Vishnu Narayanan <iamwishnu@gmail.com>
2018-03-27 08:31:04 +0530

    Add auth check for admin api's

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Server for QBUtsav Auction
 | ------ | ------ | ------ |
 |`GO_ENV`|Running environment for APP. Possible values are `development`, `production`|`development`|
 |`PORT`|`port` on which the server will listen and serve|`8080`|
+|`AUTH_TOKEN`|`auth token` to be used for authenticated routes|``|
 |`GIN_LOG_BASE_PATH`|Absolute path to the folder in which the `gin log` file must be created.WARNING: If not set then tmp folder will be used and the data will be lost when the instance restarts on normal systems|`/tmp`|
 |`GIN_LOG_FIlE_NAME`|Filename to be used for the `gin log` file|`gin.log`|
--- routes.go
+++ routes.go
@@ -14,16 +14,16 @@ func mountRoutes(app *gin.Engine) {
 	// Get team profile
 	app.GET("/teams", getTeamProfiles)
 	// Add new team
-	app.POST("/teams", createNewTeam)
+	app.POST("/teams",authenticateToken, createNewTeam)
 	// Allocate user to a team
-	app.PUT("/teams/user", allocateUserToTeam)
+	app.PUT("/teams/user", authenticateToken,allocateUserToTeam)
 
 	// Get users list
 	app.GET("/users",getUserProfiles)
 	// Lock user for auction
-	app.GET("/users/lock/:id", lockUserById)
+	app.GET("/users/lock/:id",,authenticateToken, lockUserById)
 	// Un-lock user for auction
-	app.DELETE("/users/lock/:id", unlockUserById)
+	app.DELETE("/users/lock/:id",authenticateToken, unlockUserById)
 
 	// Handle 404
 	app.NoRoute(func(c *gin.Context) {
