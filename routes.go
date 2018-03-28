26cac583870c71ca67b38075ff62ada1944853a8
nidhind <nidhind@qburst.com>
2018-03-28 09:55:33 +0530

    Changed DELETE to PUT

--- routes.go
+++ routes.go
@@ -20,14 +20,14 @@ func mountRoutes(app *gin.Engine) {
 	// Allocate user to a team
 	app.PUT("/teams/user", authenticateToken,allocateUserToTeam)
 	// de-allocate user from a team
-	app.DELETE("/teams/user", authenticateToken,deallocateUserFromTeam)
+	app.PUT("/teams/user/deallocate", authenticateToken,deallocateUserFromTeam)
 
 	// Get users list
 	app.GET("/users", authenticateToken, getUserProfiles)
 	// Lock user for auction
 	app.GET("/users/lock/:id",authenticateToken, lockUserById)
 	// Un-lock user for auction
-	app.DELETE("/users/lock/:id",authenticateToken, unlockUserById)
+	app.PUT("/users/unlock/:id",authenticateToken, unlockUserById)
 
 	// Handle 404
 	app.NoRoute(func(c *gin.Context) {
